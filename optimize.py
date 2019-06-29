46a20f86b5ea400da73c27d4930802ef57c38083
Abhijeet Malamkar <abhijeetmalamkar@gmail.com>
2019-06-29 13:52:31 -0400

    update

--- optimize.py
+++ optimize.py
@@ -7,8 +7,8 @@ np.warnings.filterwarnings('ignore')
 if __name__ == '__main__':
     trader = RLTrader()
 
-    # trader.optimize(n_trials=1)
-    # trader.train(n_epochs=1,
-    #              test_trained_model=True,
-    #              render_trained_model=True)
-    trader.test()
+    #trader.optimize(n_trials=1)
+    trader.train(n_epochs=10,
+                 test_trained_model=True,
+                 render_trained_model=True)
+    # trader.test()
